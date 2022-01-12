a65a630b21b53378d8e88fecfbb2ea7f6bddc192
eduardo de rivero <eduardomanrique@ravn.co>
2022-01-12 14:45:38 -0500

    chore: improve graphqlefieldconfig

--- src/schema/mutations/products.mutations.ts
+++ src/schema/mutations/products.mutations.ts
@@ -3,7 +3,7 @@ import { ProductService } from '../../services/products.service'
 import productInput from '../types/product.input'
 import { ProductType } from '../types/product.type'
 
-const createProduct: GraphQLFieldConfig<any, any> = {
+const createProduct: GraphQLFieldConfig<string, string> = {
   type: ProductType,
   args: { input: { type: new GraphQLNonNull(productInput) } },
   resolve: async function (_source, { input }) {
